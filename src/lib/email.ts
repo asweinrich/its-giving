8ba97495f678db0159041de7392cb138499cc459
Andrew Weinrich <asweinrich@gmail.com>
2024-11-21 06:54:08 -0800

    Update email.ts

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 
-export async function sendVerificationEmail(to: string, token: string, userId: string) {
+export async function sendVerificationEmail(to: string, token: string, userId: int) {
   //use userId for now:
   console.log(userId)
 
