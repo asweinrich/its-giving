c946676b6ca307b7efbea5e8c668e89365494658
Andrew Weinrich <asweinrich@gmail.com>
2026-05-01 10:20:04 -0700

    Update mapStyle.ts

--- src/config/mapStyle.ts
+++ src/config/mapStyle.ts
@@ -1,6 +1,6 @@
 // MapLibre GL is free — but needs a tile provider for map imagery.
 // We use MapTiler (you already have an API key).
-export const MAP_DEFAULT_STYLE = `https://api.maptiler.com/maps/streets-v2/style.json?key=M3yBnB3aobawvcom6881`;
+export const MAP_DEFAULT_STYLE = `https://api.maptiler.com/maps/019de48b-d997-7b16-a1fb-0406ac2f5262/style.json?key=M3yBnB3aobawvcom6881`;
 
 // General options for all maps
 export const MAP_DEFAULT_OPTIONS = {
